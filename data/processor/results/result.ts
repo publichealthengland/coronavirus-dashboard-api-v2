bc2fc4c9bd2d6a7d8a00eb41257f6fb49bb64491
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2020-12-19 21:13:01 +0000

    integrates area info into processor + content disposition header

--- data/processor/results/result.ts
+++ data/processor/results/result.ts
@@ -1,18 +1,24 @@
 import toCsv from "../csv";
 
-import type { GenericDBResponse, ResultProcessor } from "../../types";
+import type { GenericDBResponse, ResultProcessor, AreaInfo } from "../../types";
 
 
-const process = async ( data: GenericDBResponse, format: string, nestedMetrics: string[], releaseDate: string ) => {
+const process = async ( data: GenericDBResponse, format: string, nestedMetrics: string[], releaseDate: string, area: AreaInfo ) => {
 
-    const areaName = data?.[0]?.areaName;
+    let { areaName, areaType, areaCode } = area;
+
+    if ( areaType === "msoa" && areaCode ) {
+        areaName = data[0].areaName as string
+    }
+    
+    const fileName = `${areaName ? areaName : areaType}_${releaseDate}.${format}`
 
     switch ( format ) {
         case "csv":
             return {
                 headers: {
                     "Content-Type": "text/csv; charset=utf-8",
-                    "content-disposition": `attachment; filename="${areaName}_${releaseDate}.csv"`
+                    "content-disposition": `attachment; filename="${fileName}"`
                 },
                 body: toCsv(data, nestedMetrics)
             };
@@ -21,7 +27,7 @@ const process = async ( data: GenericDBResponse, format: string, nestedMetrics: 
             return {
                 headers: {
                     "Content-Type": "application/vnd.PHE-COVID19.v2+jsonl; charset=utf-8",
-                    "content-disposition": `attachment; filename="${areaName}_${releaseDate}.jsonl"`
+                    "content-disposition": `attachment; filename="${fileName}"`
                 },
                 body: data.map(item => JSON.stringify(item)).join("\n")
             };
@@ -30,7 +36,7 @@ const process = async ( data: GenericDBResponse, format: string, nestedMetrics: 
             return {
                 headers: {
                     "Content-Type": "application/vnd.PHE-COVID19.v2+json; charset=utf-8",
-                    "content-disposition": `attachment; filename="${areaName}_${releaseDate}.${format}"`
+                    "content-disposition": `attachment; filename="${fileName}"`
                 },
                 body: JSON.stringify({
                     length: data.length,
@@ -42,9 +48,9 @@ const process = async ( data: GenericDBResponse, format: string, nestedMetrics: 
 };  // process
 
 
-const processResults = ({ format, nestedMetrics=[], releaseDate }: ResultProcessor) => {
+const processResults = ({ format, nestedMetrics=[], releaseDate, area }: ResultProcessor) => {
 
-    return (data: GenericDBResponse) => process(data, format, nestedMetrics, releaseDate);
+    return (data: GenericDBResponse) => process(data, format, nestedMetrics, releaseDate, area);
 
 };  // processResults
 
